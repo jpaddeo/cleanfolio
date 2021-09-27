1e3a8dcaa88703cc927c21001ac9627fbffc2af5
jpaddeo <jpaddeo@gmail.com>
2021-09-27 10:03:28 -0300

    Changing homepage  on package and Footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,11 @@
-import './Footer.css'
+import './Footer.css';
 
 const Footer = () => (
   <footer className='footer'>
-    <a
-      href='https://github.com/rajshekhar26/cleanfolio'
-      className='link footer__link'
-    >
+    <a href='https://jpaddeo.github.io/portfolio' className='link footer__link'>
       Created By Juan Pablo Addeo (Forked from Raj Shekhar)
     </a>
   </footer>
-)
+);
 
-export default Footer
+export default Footer;
