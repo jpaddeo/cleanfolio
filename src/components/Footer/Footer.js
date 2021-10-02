f4fcad92c3422815c4c56dd427d913734744d093
jpaddeo <jpaddeo@gmail.com>
2021-10-01 23:39:19 -0300

    Adding copyright + fork information.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,13 @@ import './Footer.css';
 const Footer = () => (
   <footer className='footer'>
     <a href='https://jpaddeo.github.io/portfolio' className='link footer__link'>
-      Created By Juan Pablo Addeo (Forked from Raj Shekhar)
+      Created By Juan Pablo Addeo
+    </a>
+    <a
+      href='https://github.com/rajshekhar26/cleanfolio'
+      className='link footer__secondary__link'
+    >
+      (Forked from Raj Shekhar)
     </a>
   </footer>
 );
